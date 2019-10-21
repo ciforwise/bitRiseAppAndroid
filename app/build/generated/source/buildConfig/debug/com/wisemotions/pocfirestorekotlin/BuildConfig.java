b37032ad1588d47d0c4168c57b97b402ca86024d
Alessio Saggiomo <alessio.saggiomo@wisemotions.com>
2019-10-21 17:53:49 +0200

    initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/wisemotions/pocfirestorekotlin/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.wisemotions.pocfirestorekotlin;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.wisemotions.pocfirestorekotlin";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ app/src/main/java/com/wisemotions/pocfirestorekotlin/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.wisemotions.pocfirestorekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val prova: String
+
+        val db = FirebaseFirestore.getInstance()
+
+        val pippo:String
+    }
+}
