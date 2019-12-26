9aab066fa968cf3e5d38ab1c022884b6e9e4e02b
Alessio Saggiomo <alessio.saggiomo@wisemotions.com>
2019-12-26 13:18:35 +0100

    commit on pr bitrise

--- app/src/main/java/com/wisemotions/pocfirestorekotlin/MainActivity.kt
+++ app/src/main/java/com/wisemotions/pocfirestorekotlin/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val prova: String
+        val prova2: String
 
         val db = FirebaseFirestore.getInstance()
 
